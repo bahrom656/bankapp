3dfe3e57caccba9375121b5a228edb4b139a6a30
Bahrullo <bahrullo656695@gmail.com>
2020-09-17 18:02:11 +0500

    добавлен пакет card и тесты к нему

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,78 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+)
+
+const (
+	withDrawLimit = 50_000_00
+	bonusLimit    = 5_000_00
+)
+
+func IssueCard(currency types.Currency, color string, name string) types.Card {
+	card := types.Card{
+		ID:       1000,
+		PAN:      "5058 xxxx xxxx 0001",
+		Balance:  0,
+		Currency: currency,
+		Color:    color,
+		Name:     name,
+		Active:   true,
+	}
+	return card
+}
+
+func Deposit(card *types.Card, amount types.Money) {
+
+	if amount < 0 {
+		return
+	}
+	if !(*card).Active {
+		return
+	}
+	if amount > withDrawLimit {
+		return
+	}
+	(*card).Balance = amount + (*card).Balance
+}
+func AddBonus(card *types.Card, percent int, daysInMonth int, daysInYear int) {
+
+	bonus := int(card.MinBalance) * percent * daysInMonth / daysInYear / 100
+	if !card.Active {
+		return
+	}
+	if bonus > bonusLimit {
+		return
+	}
+	if (*card).Balance <= 0 {
+		return
+	}
+	(*card).Balance = (*card).Balance + types.Money(bonus)
+}
+
+func Withdraw(card types.Card, amount types.Money) types.Card {
+
+	if !card.Active {
+		return card
+	}
+	if card.Balance < amount {
+		return card
+	}
+	if amount > 20_000_00 {
+		return card
+	}
+	if amount < 0 {
+		return card
+	}
+	card.Balance = card.Balance - amount
+	return card
+}
+func Total(cards []types.Card) types.Money {
+	var sum types.Money
+	for _, card := range cards {
+		if card.Active == true && card.Balance >= 0{
+			sum = sum + card.Balance
+		}
+	}
+	return sum
+}
---
+++ pkg/bank/card/example_test.go
@@ -0,0 +1,22 @@
+package card
+
+import (
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+func ExampleTotal() {
+	cards := []types.Card{
+		{
+			Balance: 10_000_00,
+			Active:  true,
+		},
+		{
+			Balance: 20_000_00,
+			Active:  true,
+		},
+	}
+	fmt.Println(Total(cards))
+	//Output:
+	//3000000
+}
